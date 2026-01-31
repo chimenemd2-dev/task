121b63c2cd52627c9dd50f8891536ef636aa48a3
chimenem <chimenemd2@gmail.com>
2026-01-30 19:15:30 -0800

    initial comit

---
+++ script.js
@@ -0,0 +1,8 @@
+// btn1 =  document.getElementsByClassName("btn")
+// btn1.addEventListener("click", function hello() {
+//     alert("Hello Chimenem")
+// })
+
+function Explore(params) {
+    alert("Explore Now!!!")
+}
